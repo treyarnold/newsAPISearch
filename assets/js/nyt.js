20021a46bcd7d509d9fef408ffad8d9f59d0eb60
treyarnold <arnold.trey80@gmail.com>
2019-03-30 14:04:52 -0500

    articles showing. dates are ignored

--- assets/js/nyt.js
+++ assets/js/nyt.js
@@ -1,50 +1,39 @@
 const app = {
   searchProperties: {},
 
-  search: function (searchProp) {
+  search: function (queryURL){
+    var getEm = 'https://newsapi.org/v2/everything?' +
+              'q=' + queryURL +
+              '&apiKey=81f15c72b7b34ec0a2c3480c6aaf5db0';
+    $.ajax({
+        url: getEm,
+        method: "GET"
+    }).then (response =>{
+        this.showArticles(response);
+    })
+  },
 
+  showArticles: function (response) {
+    $("#articleDiv").empty();
+    let articleCount = this.searchProperties.recordCount;
+    for (let i = 0; i < articleCount; i++) {
+      let article = response.articles[i]
+      let display =   
+        `<div>
+          <a href="${article.url}" target="_blank"><h2>${article.title}</h2></a>
+          <p><em>${article.source.name}</em</p>
+          <p>${article.description}</p>
+        </div>`
+      $("#articleDiv").prepend(display);
+    }
   }
 }
 
 $(document).ready(function(){
-
- var search = $("#searchTerm").val();
-
-//  $("#searchTerm").keyup(function (event) {
-//     if (event.keyCode === 13) {
-//         var search = $("#searchTerm").val();
-//         searchNews(search);
-//         event.preventDefault();
-//         // gifs.push(search);
-//         search = "";
-//         // buttons();
-//     }
-// });
-
-// function searchNews(search){
-
-// }
-
-// var url = 'https://newsapi.org/v2/everything?' +
-//           'q=' + query +
-//           '&from=2019-03-30&' +
-//           'sortBy=popularity&' +
-//           'apiKey=81f15c72b7b34ec0a2c3480c6aaf5db0';
-
-// var req = new Request(url);
-
-// fetch(req)
-//     .then(function(response) {
-//         console.log(response.json());
-//     })
-
-
-// });
-
-$("#search").on("click", function (event){
-  event.preventDefault();
-  app.searchProperties.searchTerm = $("#searchTerm").val();
-  app.searchProperties.recordCount = $("#recordCount").val();
-  app.search(app.searchProperties);
-});
+  $("#search").on("click", function (event){
+    event.preventDefault();
+    app.searchProperties.searchTerm = $("#searchTerm").val();
+    app.searchProperties.recordCount = $("#recordCount").val();
+    app.search(app.searchProperties.searchTerm);
+  });
 });
