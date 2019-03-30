8e8765f46096a2f1433d8e231cb4850e374d26ed
treyarnold <arnold.trey80@gmail.com>
2019-03-30 13:05:16 -0500

    moved js to folder

---
+++ assets/js/nyt.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+ var search = $("#searchTerm").val();
+
+//  $("#searchTerm").keyup(function (event) {
+//     if (event.keyCode === 13) {
+//         var search = $("#searchTerm").val();
+//         searchNews(search);
+//         event.preventDefault();
+//         // gifs.push(search);
+//         search = "";
+//         // buttons();
+//     }
+// });
+
+function searchNews(search){
+
+}
+
+var url = 'https://newsapi.org/v2/everything?' +
+          'q=' + query +
+          '&from=2019-03-30&' +
+          'sortBy=popularity&' +
+          'apiKey=81f15c72b7b34ec0a2c3480c6aaf5db0';
+
+var req = new Request(url);
+
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
+
+
+});
